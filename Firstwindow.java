e8adbcc1096912fb899515376569f367f5266847
hghebrem <hghebrem@mix.wvu.edu>
2016-07-05 23:27:23 -0400

    Third Commit

--- Firstwindow.java
+++ Firstwindow.java
@@ -26,11 +26,12 @@ public class Firstwindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	
-	/// 2nd Com 
-	
+
+
 	
 public Firstwindow() {
 	super(" This is the title of the window"); // the super is just refering to the class which we are extending which is JFrame
+	
 	
 	setSize(600, 400);// 600 by 400 pixels window size
 	
@@ -44,21 +45,18 @@ public Firstwindow() {
 	JButton b = new JButton("Button 1");	
 	JButton d = new JButton("Button 2");
 	
-	d.addActionListener(new ActionListener() {
-		
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
-		
-			JOptionPane.showMessageDialog(null,"well done btw this is the Message parameter"+ e); // if you get rid of e u get just the message ....lesson 34
-			
-		}
+	
 
 		
-	});
 	
-	JCheckBox cb = new JCheckBox("Whatsup,This is a Check Box");	
-	JCheckBox cb2 = new JCheckBox("Not much");
+	
+JCheckBox cb = new JCheckBox("This is a CheckBox cb");	
+JCheckBox cb2 = new JCheckBox("This is CheckBox cb2");
+
+
+	
+	
+	
 	
 	JTextArea ta = new JTextArea("This is a text are");
 	JTextField tf = new JTextField("This is a text field");
@@ -93,8 +91,58 @@ public Firstwindow() {
 	add(p2,BorderLayout.CENTER);  //using BorderLayout you can orient where you want your components
 	add(p3,BorderLayout.NORTH);
 	
-}
+	
+	
+	
+	d.addActionListener(new ActionListener() {   // Button 2 is defined as d and the addActionListener is designed for button 2
+		
+		public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+		
+if(cb.isSelected()){
+			
+	System.out.println("cb is selected");
+	JOptionPane.showMessageDialog(null,"cb is selected");
+	
+	
+	
+	
+	if(cb2.isSelected()){
+						
+			System.out.println("cb2 is selected");
 
 
+	
 }
+		
+}
+	
+		
+			
+		
+			JOptionPane.showMessageDialog(null,"this is the message that pops up for Button 2"); // if you get rid of e u get just the message ....lesson 34......s is a string we defined up top
+			
+		}
+
+		
+	});
+	
+	
+	
+	
+	
+	
+
+}
+}
+
+	
+	
+
+
+
+
+
+
+
 
--- Mainmeth.java
+++ Mainmeth.java
@@ -5,10 +5,14 @@ public static void main(String arg[]){
 	
 	
 	
-	for( int i = 0; i< 2; i++){
+	for( int i = 0; i< 1; i++){
 	Firstwindow fw = new Firstwindow(); //.....It will create  2 windows...
 	
 	fw.setVisible(true);
 }
 }
 }
+
+
+
+	
